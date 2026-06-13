3f7ddcfd81bbfb226b6b192e8056cdf1f7bfcb8f
Lautaro <lautarovanh@gmail.com>
2026-06-13 15:53:47 -0300

    se implemento el interceptor loadingtime

--- src/main/java/com/lautaro/curso/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/lautaro/curso/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,7 @@
 package com.lautaro.curso.app.interceptor.springboot_interceptor.interceptors;
 
+import java.util.Random;
+
 import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,20 +19,33 @@ public class LoadingTimeInterceptor implements HandlerInterceptor{
     private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-            @Nullable ModelAndView modelAndView) throws Exception {
-                HandlerMethod controllerHandlerMethod = (HandlerMethod) handler;
-                logger.info("Loading time interceptor: postHandler quiting " + controllerHandlerMethod.getMethod().getName() + "...");
-        
-    }
-
-    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
                 HandlerMethod controllerHandlerMethod = (HandlerMethod) handler;
         logger.info("Loading time interceptor: preHandler executing " + controllerHandlerMethod.getMethod().getName() + "...");
+
+        long start = System.currentTimeMillis();
+        request.setAttribute("startTime", start);
+
+        Random random = new Random();
+        int delay = random.nextInt(500);
+        Thread.sleep(delay);
         return true;
     }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable ModelAndView modelAndView) throws Exception {
+        HandlerMethod controllerHandlerMethod = (HandlerMethod) handler;
+
+        long end = System.currentTimeMillis();
+        long start = (Long) request.getAttribute("startTime");
+        long loadingTime = end - start;
+        logger.info("Loading time interceptor: postHandler quiting " + controllerHandlerMethod.getMethod().getName() + "...");
+        logger.info("Loading time: " + loadingTime + " ms");
+        logger.info("Tiempo transcurrido " + loadingTime);
+    }
+
 
 
 
