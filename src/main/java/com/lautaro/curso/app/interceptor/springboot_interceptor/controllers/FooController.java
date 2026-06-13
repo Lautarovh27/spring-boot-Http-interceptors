85be240bb9162242b31d5b9222c4db79866cf70c
Lautaro <lautarovanh@gmail.com>
2026-06-13 15:42:49 -0300

    Se implemento el restcontroller

---
+++ src/main/java/com/lautaro/curso/app/interceptor/springboot_interceptor/controllers/FooController.java
@@ -0,0 +1,32 @@
+package com.lautaro.curso.app.interceptor.springboot_interceptor.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping("/app")
+public class FooController {
+
+    @GetMapping("/foo")
+    public Map<String, String> foo(){
+        return Collections.singletonMap("message", "Handler foo del controller ");
+    }
+    
+     @GetMapping("/bar")
+    public Map<String, String> bar(){
+        return Collections.singletonMap("message", "Handler bar del controller ");
+    }
+
+     @GetMapping("/baz")
+    public Map<String, String> baz(){
+        return Collections.singletonMap("message", "Handler baz del controller ");
+    }
+}
--- src/main/java/com/lautaro/curso/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/lautaro/curso/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -4,6 +4,7 @@ import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,14 +19,16 @@ public class LoadingTimeInterceptor implements HandlerInterceptor{
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             @Nullable ModelAndView modelAndView) throws Exception {
-                    logger.info("Loading time interceptor: postHandler quiting");
+                HandlerMethod controllerHandlerMethod = (HandlerMethod) handler;
+                logger.info("Loading time interceptor: postHandler quiting " + controllerHandlerMethod.getMethod().getName() + "...");
         
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        logger.info("Loading time interceptor: preHandler executing");
+                HandlerMethod controllerHandlerMethod = (HandlerMethod) handler;
+        logger.info("Loading time interceptor: preHandler executing " + controllerHandlerMethod.getMethod().getName() + "...");
         return true;
     }
 
